594558aca325f4e8087d766bd8158a20e91a8275
rosyk <rosyknikita@gmail.com>
2023-05-01 13:42:07 +0300

    task 11

--- rosyk_task11.py
+++ rosyk_task11.py
@@ -3,26 +3,47 @@ import re
 
 def find_address(line):
     address_pattern = r'(адреса|адр\.*)\s+(вул\.*|вулиця)\s+[a-zA-Zа-яА-Яєії\' ]+,\s+\d+\s+((кв\.*)\s+\d+)*'
-    address = re.match(address_pattern, line)
+    address = re.search(address_pattern, line)
     return address.group(0) if address else 'no address'
 
 
 def find_name(line):
-    name_pattern = r'[a-zA-Zа-яА-Яєії\']+\s+[a-zA-Zа-яА-Яєії\'\.]+\s+[a-zA-Zа-яА-Яєії\'\.]+'
-    name = re.match(name_pattern, line)
+    name_pattern = r'[А-ЯЄІЇ][а-яєії]+\s+[А-ЯЄІЇа-яєії.]+\s+[А-ЯЄІЇа-яєії.]+'
+    name = re.search(name_pattern, line)
     return name.group(0) if name is not None else 'no name'
 
 
 def find_debt(line):
     debt_pattern = r'\d+$'
-    debt = re.match(debt_pattern, line)
+    debt = re.search(debt_pattern, line)
     return debt.group(0) if debt is not None else 'no debt'
 
 
 def find_phone(line):
     phone_pattern = r'(телефон|тел\.*)+\s+(\+*38)*0\d{2} *\d{3} *\d{2} *\d{2}'
-    phone = re.match(phone_pattern, line)
-    return phone if phone is not None else 'no phone'
+    phone = re.search(phone_pattern, line)
+    return phone.group(0) if phone is not None else 'no phone'
+
+
+def find_date(line):
+    date_pattern = r'(\d{1,2}[.\-/]\d{1,2}[.\-/]\d{4})|(\d{4}[.\-/]\d{1,2}[.\-/]\d{1,2})'
+    date = re.search(date_pattern, line)
+    return date.group(0) if date is not None else 'no date'
+
+
+def find_student_phone(line):
+    student_phone_pattern = r'(\+*38)*0\d{2}\d{3}\d{2}\d{2}'
+    student_phone = re.search(student_phone_pattern, line)
+    if student_phone is not None:
+        return '+380' + student_phone.group(0)[-9:]
+    else:
+        return 'no phone'
+
+
+def find_number(line):
+    number_pattern = r'\d{5}'
+    number = re.search(number_pattern, line)
+    return number.group(0) if number is not None else 'no number'
 
 
 def create_letter():
@@ -33,8 +54,18 @@ def create_letter():
                      f'The amount of your debt for services is {find_debt(line)}.\n' \
                      f'Please pay the debt within a month. ' \
                      f'Otherwise, the provision of services will be discontinued.\n\n'
-        letters.write(letter)
+            letters.write(letter)
+
+
+def create_dict():
+    students_dict = {}
+    with open('students.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            student_information = f'{find_name(line)}, {find_date(line)}, {find_student_phone(line)}'
+            students_dict.update({find_number(line): student_information})
+    return students_dict
 
 
 if __name__ == '__main__':
     create_letter()
+    print(create_dict())
---
+++ tests/unit/test_task11.py
@@ -0,0 +1,31 @@
+import pytest
+import os
+from rosyk_task11 import find_student_phone, create_letter, create_dict
+from unittest.mock import mock_open, call
+
+
+@pytest.mark.parametrize('input_param, expected', [('str+380991234567str', '+380991234567'),
+                                                   ('str380991234567str', '+380991234567'),
+                                                   ('str0991234567str', '+380991234567')])
+def test_find_student_phone(input_param, expected):
+    assert find_student_phone(input_param) == expected
+
+
+# @pytest.mark.parametrize('input_param, expected', [('адр вул. Лесі Українки, 13 кв 56 Симоненко В. К. тел 380991234567 10000',
+#                                                     '''тел 380991234567, адр вул. Лесі Українки, 13 кв 56
+# Dear Симоненко В. К.
+# The amount of your debt for services is 10000.
+# Please pay the debt within a month. Otherwise, the provision of services will be discontinued.''')])
+# def test_create_letter(input_param, expected, monkeypatch):
+#     monkeypatch.setattr('builtins.open', fake_open('people.txt'))
+#     create_letter()
+#     with open('letters.txt', 'r', encoding='utf-8') as f:
+#         print(f.read())
+#         assert f.read() == expected
+
+
+@pytest.mark.parametrize('input_param, expected', [('Михайличенко Андрій Степанович 12345 01.01.2000 +380991234567',
+                                                    {'12345': 'Михайличенко Андрій Степанович, 01.01.2000, +380991234567'})])
+def test_create_dict(input_param, expected, monkeypatch):
+    monkeypatch.setattr('builtins.open', mock_open(read_data=input_param))
+    assert create_dict() == expected
