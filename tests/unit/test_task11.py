8e420e96593dae324960008169dec92fe00db256
rosyk <rosyknikita@gmail.com>
2023-05-03 20:29:41 +0300

    task 11

--- tests/unit/test_task11.py
+++ tests/unit/test_task11.py
@@ -11,17 +11,17 @@ def test_find_student_phone(input_param, expected):
     assert find_student_phone(input_param) == expected
 
 
-# @pytest.mark.parametrize('input_param, expected', [('адр вул. Лесі Українки, 13 кв 56 Симоненко В. К. тел 380991234567 10000',
-#                                                     '''тел 380991234567, адр вул. Лесі Українки, 13 кв 56
-# Dear Симоненко В. К.
-# The amount of your debt for services is 10000.
-# Please pay the debt within a month. Otherwise, the provision of services will be discontinued.''')])
-# def test_create_letter(input_param, expected, monkeypatch):
-#     monkeypatch.setattr('builtins.open', fake_open('people.txt'))
-#     create_letter()
-#     with open('letters.txt', 'r', encoding='utf-8') as f:
-#         print(f.read())
-#         assert f.read() == expected
+@pytest.mark.parametrize('input_param, expected', [('адр вул. Лесі Українки, 13 кв 56 Симоненко В. К. тел 380991234567 10000',
+                                                    '''тел 380991234567, адр вул. Лесі Українки, 13 кв 56
+Dear Симоненко В. К.
+The amount of your debt for services is 10000.
+Please pay the debt within a month. Otherwise, the provision of services will be discontinued.''')])
+def test_create_letter(input_param, expected, monkeypatch):
+    monkeypatch.setattr('builtins.open', mock_open(read_data=input_param))
+    create_letter()
+    with open('letters.txt', 'r', encoding='utf-8') as f:
+        print(f.read())
+        assert f.read() == expected
 
 
 @pytest.mark.parametrize('input_param, expected', [('Михайличенко Андрій Степанович 12345 01.01.2000 +380991234567',
