11b09dfbeba243d4f5415539b02507942dd089af
rosyk <rosyknikita@gmail.com>
2023-05-06 15:27:17 +0300

    lab 10

--- tests/unit/test_task10.py
+++ tests/unit/test_task10.py
@@ -1,11 +1,11 @@
 import pytest
 from printing_imitation import print_models, show_completed_models
 
-
+INT_LIST = [-1, 0, 1]
 @pytest.mark.parametrize('unprinted_designs, completed_models, expected',
                          [(['second design', 'first design'],
                            [], 'Printing model: first design\nPrinting model: second design'),
-                          ([-1, 0, 1], [],
+                          (INT_LIST.copy(), [],
                            'Printing model: 1\nPrinting model: 0\nPrinting model: -1'),
                           ([], [], '')])
 def test_print_models(unprinted_designs, completed_models, expected, capsys):
@@ -17,7 +17,7 @@ def test_print_models(unprinted_designs, completed_models, expected, capsys):
 @pytest.mark.parametrize('completed_models, expected',
                          [(['first model', 'second model'],
                            'The following models have been printed:\nfirst model\nsecond model'),
-                          ([-1, 0, 1], 'The following models have been printed:\n-1\n0\n1'),
+                          (INT_LIST, 'The following models have been printed:\n-1\n0\n1'),
                           ([], 'The following models have been printed:')])
 def test_show_completed_models(completed_models, expected, capsys):
     show_completed_models(completed_models)
