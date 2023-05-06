cbc986a331320f7b718e16676705d1f1d246be11
rosyk <rosyknikita@gmail.com>
2023-05-06 15:33:57 +0300

    refactor test for lab 10

--- tests/unit/test_task10.py
+++ tests/unit/test_task10.py
@@ -1,12 +1,10 @@
 import pytest
 from printing_imitation import print_models, show_completed_models
 
-INT_LIST = [-1, 0, 1]
+
 @pytest.mark.parametrize('unprinted_designs, completed_models, expected',
                          [(['second design', 'first design'],
                            [], 'Printing model: first design\nPrinting model: second design'),
-                          (INT_LIST.copy(), [],
-                           'Printing model: 1\nPrinting model: 0\nPrinting model: -1'),
                           ([], [], '')])
 def test_print_models(unprinted_designs, completed_models, expected, capsys):
     print_models(unprinted_designs, completed_models)
@@ -17,7 +15,6 @@ def test_print_models(unprinted_designs, completed_models, expected, capsys):
 @pytest.mark.parametrize('completed_models, expected',
                          [(['first model', 'second model'],
                            'The following models have been printed:\nfirst model\nsecond model'),
-                          (INT_LIST, 'The following models have been printed:\n-1\n0\n1'),
                           ([], 'The following models have been printed:')])
 def test_show_completed_models(completed_models, expected, capsys):
     show_completed_models(completed_models)
