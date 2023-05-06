d09b32db0f626a0f82bc957b2b76b2cfd1a25301
rosyk <rosyknikita@gmail.com>
2023-05-06 16:55:47 +0300

    refactor tests for task 7

--- tests/unit/test_task7.py
+++ tests/unit/test_task7.py
@@ -2,51 +2,38 @@ import pytest
 from rosyk_task7 import Point, Vector
 
 
+FIRST_VECTOR = Vector(Point(0, 0), Point(5, 8))
+SECOND_VECTOR = Vector(Point(9, 1), Point(6, 3))
+ZERO_VECTOR = Vector(Point(0, 0), Point(0, 0))
+
+
 def test_vector_init():
-    vector = Vector(Point(0, 0), Point(5, 8))
+    vector = FIRST_VECTOR
     assert vector.length == pytest.approx(9.433, 0.001)
     assert vector.x == 5
     assert vector.y == 8
 
 
-@pytest.fixture()
-def return_first_vector():
-    return Vector(Point(0, 0), Point(5, 8))
-
-
-@pytest.fixture()
-def return_second_vector():
-    return Vector(Point(9, 1), Point(6, 3))
-
-
-@pytest.fixture()
-def return_zero_vector():
-    return Vector(Point(0, 0), Point(0, 0))
 
 
 @pytest.mark.parametrize('vectors, expected',
-                         [(['return_first_vector', 'return_second_vector'],
+                         [([FIRST_VECTOR, SECOND_VECTOR],
                            {'x': 2, 'y': 10}),
-                          (['return_zero_vector', 'return_zero_vector'],
+                          ([ZERO_VECTOR, ZERO_VECTOR],
                            {'x': 0, 'y': 0})])
-def test_vector_addition(vectors, expected, request):
-    first_vector = request.getfixturevalue(vectors[0])
-    second_vector = request.getfixturevalue(vectors[1])
-
-    result = first_vector + second_vector
+def test_vector_addition(vectors, expected):
+    result = vectors[0] + vectors[1]
     assert result.x == expected['x']
     assert result.y == expected['y']
 
 
 @pytest.mark.parametrize('vectors, expected',
-                         [(['return_first_vector', 'return_second_vector'],
+                         [([FIRST_VECTOR, SECOND_VECTOR],
                            {'x': 8, 'y': 6}),
-                          (['return_zero_vector', 'return_zero_vector'],
+                          ([ZERO_VECTOR, ZERO_VECTOR],
                            {'x': 0, 'y': 0})])
-def test_vector_subtraction(vectors, expected, request):
-    first_vector = request.getfixturevalue(vectors[0])
-    second_vector = request.getfixturevalue(vectors[1])
-
-    result = first_vector - second_vector
+def test_vector_subtraction(vectors, expected):
+    result = vectors[0] - vectors[1]
+    print(result.x, result.y)
     assert result.x == expected['x']
     assert result.y == expected['y']
